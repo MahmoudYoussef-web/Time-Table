02e2f5259e2291e66bb30fb5eb096db2633a133a
MahmoudYoussef-web <lime24864@gmail.com>
2026-06-27 20:02:25 +0300

    chore: remove unused students and enrollments modules

--- frontend/src/api/enrollments.ts
+++
@@ -1,8 +0,0 @@
-import client from './client';
-import { Enrollment, EnrollmentRequest } from '../types';
-
-export const getEnrollments    = ()                             => client.get<Enrollment[]>('/enrollments').then(r => r.data);
-export const getEnrollment     = (id: number)                   => client.get<Enrollment>(`/enrollments/${id}`).then(r => r.data);
-export const createEnrollment  = (data: EnrollmentRequest)      => client.post<Enrollment>('/enrollments', data).then(r => r.data);
-export const updateEnrollment  = (id: number, data: EnrollmentRequest) => client.put<Enrollment>(`/enrollments/${id}`, data).then(r => r.data);
-export const deleteEnrollment  = (id: number)                   => client.delete(`/enrollments/${id}`);
